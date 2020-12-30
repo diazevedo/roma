a869aa31fdb5f911419f12c7da4063cc3ec67527
Diego Azevedo <dapptn@hotmail.com>
2020-12-31 00:41:45 +1100

    update relationships

--- backend/extensions/users-permissions/config/jwt.js
+++ backend/extensions/users-permissions/config/jwt.js
@@ -1,3 +1,4 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '2eef72aa-0e1d-463b-9730-174dcf4a162c'
+  expirensIn: "1d",
+  jwtSecret: process.env.JWT_SECRET || "2eef72aa-0e1d-463b-9730-174dcf4a162c",
 };
